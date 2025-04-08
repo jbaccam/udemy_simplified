e911382d2da8b4f436e419567537958160b600c8
Pranava Sai Maganti <pranava7@iastate.edu>
2025-04-08 14:12:43 +0000

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import BrowseCourses from './components/Browse';
+import Cart from './components/Cart';
+import Payment from './components/Payment';
+import Summary from './components/Summary';
+
+const App = () => {
+  const [cart, setCart] = useState([]);
+  const [step, setStep] = useState("browse");
+
+  return (
+    <div className="app-container">
+      {step === "browse" && <BrowseCourses cart={cart} setCart={setCart} setStep={setStep} />}
+      {step === "cart" && <Cart cart={cart} setCart={setCart} setStep={setStep} />}
+      {step === "payment" && <Payment cart={cart} setCart={setCart} setStep={setStep} />}
+      {step === "summary" && <Summary cart={cart} setStep={setStep} />}
+    </div>
+  );
+};
+
+export default App;
